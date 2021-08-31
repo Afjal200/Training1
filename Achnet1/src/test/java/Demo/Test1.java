4a74417ea16feadc067450ec0e720e72ea4b3623
Admin <Admin@DESKTOP-TVENK6L>
2021-08-31 18:42:07 +0530

    first commit

---
+++ Achnet1/src/test/java/Demo/Test1.java
@@ -0,0 +1,18 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
